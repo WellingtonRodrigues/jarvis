ea5d4d138d81d6ee0cc98780d3fc99eb874721ab
sergioaugrod <sergioaugrod@gmail.com>
2015-05-20 20:05:47 -0300

    Start express, rm node_modules and .gitignore

--- src/app/modules/app/app.js
+++ src/app/modules/app/app.js
@@ -1,10 +1,15 @@
 var express = require('express');
-var server =
+var server = express();
+var http = require('http').Server(app);
+
+var PORT = 8500;
 
 var app = {
-  start: function() {
-
-  }
+    start: function() {
+        http.listen(PORT, function() {
+            console.log('JARVIS RUNNING ON: ' + PORT);
+        });
+    }
 };
 
 module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+var app = require('./app/modules/app/app.js');
+
+app.start();
